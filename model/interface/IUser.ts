3460e09d7d495c790ea72e1aa710a12409b9a068
wook <gus5427@naver.com>
2022-04-17 20:52:28 +0900

    Feat : create account interface

---
+++ model/interface/IUser.ts
@@ -0,0 +1,45 @@
+export default interface IUser {
+  accountDetail: {
+    account: {
+      birthDay: string;
+      bloodType: { abo: string; rh: string };
+      createdAt: string;
+      displayName: string;
+      email: string;
+      fcmToken: string | null;
+      gender: string;
+      marketingUserAgreement: boolean;
+      modifiedAt: string;
+      oauthUuid: string;
+      phoneNumber: string;
+      privacyPolicy: boolean;
+      profileImageUrl: string;
+      providerType: string;
+      role: string;
+      status: string;
+      termOfService: boolean;
+      uuid: string;
+    };
+    inboxThanksLetters: Array<any>;
+    sendThanksLetters: Array<any>;
+    reportCount: number;
+  };
+  birthDay: string;
+  bloodType: { abo: string; rh: string };
+  createdAt: string;
+  displayName: string;
+  email: string;
+  fcmToken: string | null;
+  gender: string;
+  marketingUserAgreement: boolean;
+  modifiedAt: string;
+  oauthUuid: string;
+  phoneNumber: string;
+  privacyPolicy: boolean;
+  profileImageUrl: string;
+  providerType: string;
+  role: string;
+  status: string;
+  termOfService: boolean;
+  uuid: string;
+}

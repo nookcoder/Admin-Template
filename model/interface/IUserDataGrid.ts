4da00fa9cf8887f2145e8340acee9b6089f16fcf
wook <gus5427@naver.com>
2022-04-17 22:32:59 +0900

    Feat : add IUserDataGrid.ts

---
+++ model/interface/IUserDataGrid.ts
@@ -0,0 +1,13 @@
+export default interface IUserDataGrid {
+  id: number;
+  birthDay: string;
+  displayName: string;
+  // bloodType: { abo: string; rh: string };
+  createdAt: string;
+  email: string;
+  gender: string;
+  marketingUserAgreement: boolean;
+  phoneNumber: string;
+  status: string;
+  uuid: string;
+}

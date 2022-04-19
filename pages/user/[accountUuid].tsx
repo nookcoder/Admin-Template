7046348e6554b86c13cc1b220ef0d6de937c98d6
wook <gus5427@naver.com>
2022-04-20 01:20:51 +0900

    Feat : create [accountUuid].tsx

---
+++ pages/user/[accountUuid].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NextPage } from "next";
+
+const AccountUuid: NextPage = () => {
+  return (
+    <div>
+      <span>hello</span>
+    </div>
+  );
+};
+
+export default AccountUuid;

3ecdb4e907428a16280b70a78dba926c858700eb
wook <gus5427@naver.com>
2022-04-20 19:53:24 +0900

    Refactor : change algorithm to set userInformation in index.tsx

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalNavigationBar from "../../components/common/GlobalNavigationBar";
 import Head from "next/head";
 import { NextPage } from "next";
@@ -14,14 +14,37 @@ import {
 import { ClickType } from "../../util/constant";
 import { routePageByUuid } from "../../hooks/routerHook";
 import { useRouter } from "next/router";
+import { CircularProgress } from "@mui/material";
+import { fetchData } from "../../api/basicFetch";
+import { PrintErrorMessage } from "../../util/Error";
 
 // @ts-ignore
-const User: NextPage = ({ data }) => {
+const User: NextPage = () => {
   const router = useRouter();
-  const [initData, setInitData] = useState<IUser[]>(data);
-  const [userInformation, setUserInformation] = useState<Array<IUserDataGrid>>(
-    [],
-  );
+  const [initData, setInitData] = useState<IUser[]>();
+  const [userInformation, setUserInformation] =
+    useState<Array<IUserDataGrid>>();
+
+  const createUserInformation = (initData: IUser[]) => {
+    const userInformation: Array<IUserDataGrid> = [];
+    initData.map((data, index) => {
+      const user: IUserDataGrid = {
+        id: index,
+        uuid: data.uuid,
+        createdAt: data.createdAt,
+        birthDay: data.birthDay,
+        displayName: data.displayName,
+        status: data.status,
+        phoneNumber: data.phoneNumber,
+        marketingUserAgreement: data.marketingUserAgreement,
+        gender: data.gender,
+        email: data.email,
+      };
+      userInformation.push(user);
+    });
+    setUserInformation([...userInformation]);
+  };
+
   const dataColumn: GridColDef[] = [
     { field: "displayName", headerName: "닉네임", width: 200 },
     { field: "email", headerName: "이메일", width: 200 },
@@ -35,31 +58,7 @@ const User: NextPage = ({ data }) => {
     { field: "uuid", headerName: "사용자 식별자", width: 300 },
   ];
 
-  const dataRow: GridRowsProp = [...userInformation];
-
-  const initUserInformation = useCallback(() => {
-    let newUserInformationArray: Array<IUserDataGrid> = [];
-    initData.map((user, key) => {
-      let userInfo: IUserDataGrid = {
-        id: key,
-        birthDay: user.birthDay,
-        displayName: user.displayName,
-        // bloodType: {
-        //   abo: user.bloodType.abo,
-        //   rh: user.bloodType.rh,
-        // },
-        createdAt: user.createdAt.slice(0, 10),
-        email: user.email,
-        gender: user.gender,
-        marketingUserAgreement: user.marketingUserAgreement,
-        phoneNumber: user.phoneNumber,
-        status: user.status,
-        uuid: user.uuid,
-      };
-      newUserInformationArray.push(userInfo);
-    });
-    setUserInformation([...newUserInformationArray]);
-  }, [initData]);
+  const dataRow: GridRowsProp = userInformation ? [...userInformation] : [];
 
   const onCellClick = (
     params: GridCellParams<any>,
@@ -71,8 +70,15 @@ const User: NextPage = ({ data }) => {
   };
 
   useEffect(() => {
-    initUserInformation();
-  }, []);
+    if (!initData) {
+      fetchData("/api/v1/account/all", setInitData).catch((err) => {
+        PrintErrorMessage(err);
+      });
+    }
+    if (initData) {
+      createUserInformation(initData);
+    }
+  }, [initData]);
 
   // 기본 회원 정보, 감사 메세지 수 , 좋/댓/공, 수혈완료, 포인트
   return (
@@ -83,21 +89,18 @@ const User: NextPage = ({ data }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GlobalNavigationBar />
-      <DataGrid
-        columns={dataColumn}
-        rows={dataRow}
-        autoHeight={true}
-        onCellClick={onCellClick}
-      />
+      {userInformation ? (
+        <DataGrid
+          columns={dataColumn}
+          rows={dataRow}
+          autoHeight={true}
+          onCellClick={onCellClick}
+        />
+      ) : (
+        <CircularProgress />
+      )}
     </>
   );
 };
 
-export async function getServerSideProps() {
-  const res = await fetch(`${process.env.BASE_URL}api/v1/account/all`);
-  const data: IUser[] = await res.json();
-  return {
-    props: { data },
-  };
-}
 export default User;

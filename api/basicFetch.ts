b32d69ff12ed45ab052473b569bfe59074d051f8
wook <gus5427@naver.com>
2022-04-20 20:41:47 +0900

    Refactor : refactor initUserData

--- api/basicFetch.ts
+++ api/basicFetch.ts
@@ -1,9 +1,29 @@
 import React from "react";
+import IUser from "../model/interface/IUser";
 
 export function fetchData(url: string, setData: React.Dispatch<any>) {
   return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`).then((res) => {
     res.json().then((value) => {
       setData(value);
+    });
+  });
+}
+
+export function fetchInitUserData(
+  url: string,
+  setData: React.Dispatch<IUser[]>,
+) {
+  return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`).then((res) => {
+    res.json().then((value: IUser[]) => {
+      const initData: Array<IUser> = [];
+      value.map((data, index) => {
+        const user = {
+          ...data,
+          id: index,
+        };
+        initData.push(user);
+      });
+      setData(initData);
     });
   });
 }
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -3,7 +3,6 @@ import GlobalNavigationBar from "../../components/common/GlobalNavigationBar";
 import Head from "next/head";
 import { NextPage } from "next";
 import IUser from "../../model/interface/IUser";
-import IUserDataGrid from "../../model/interface/IUserDataGrid";
 import {
   DataGrid,
   GridCellParams,
@@ -11,54 +10,21 @@ import {
   GridRowsProp,
   MuiEvent,
 } from "@mui/x-data-grid";
-import { ClickType } from "../../util/constant";
+import { ClickType, GridColumn } from "../../util/constant";
 import { routePageByUuid } from "../../hooks/routerHook";
 import { useRouter } from "next/router";
 import { CircularProgress } from "@mui/material";
-import { fetchData } from "../../api/basicFetch";
+import { fetchInitUserData } from "../../api/basicFetch";
 import { PrintErrorMessage } from "../../util/Error";
 
 // @ts-ignore
 const User: NextPage = () => {
   const router = useRouter();
   const [initData, setInitData] = useState<IUser[]>();
-  const [userInformation, setUserInformation] =
-    useState<Array<IUserDataGrid>>();
 
-  const createUserInformation = (initData: IUser[]) => {
-    const userInformation: Array<IUserDataGrid> = [];
-    initData.map((data, index) => {
-      const user: IUserDataGrid = {
-        id: index,
-        uuid: data.uuid,
-        createdAt: data.createdAt,
-        birthDay: data.birthDay,
-        displayName: data.displayName,
-        status: data.status,
-        phoneNumber: data.phoneNumber,
-        marketingUserAgreement: data.marketingUserAgreement,
-        gender: data.gender,
-        email: data.email,
-      };
-      userInformation.push(user);
-    });
-    setUserInformation([...userInformation]);
-  };
+  const dataColumn: GridColDef[] = GridColumn.User;
 
-  const dataColumn: GridColDef[] = [
-    { field: "displayName", headerName: "닉네임", width: 200 },
-    { field: "email", headerName: "이메일", width: 200 },
-    { field: "phoneNumber", headerName: "전화번호", width: 120 },
-    { field: "birthDay", headerName: "생년월일" },
-    // { field: "bloodType", headerName: "혈액형" },
-    { field: "gender", headerName: "성별" },
-    { field: "marketingUserAgreement", headerName: "마케팅활용동의" },
-    { field: "status", headerName: "상태" },
-    { field: "createdAt", headerName: "가입날짜" },
-    { field: "uuid", headerName: "사용자 식별자", width: 300 },
-  ];
-
-  const dataRow: GridRowsProp = userInformation ? [...userInformation] : [];
+  const dataRow: GridRowsProp = initData ? [...initData] : [];
 
   const onCellClick = (
     params: GridCellParams<any>,
@@ -71,12 +37,10 @@ const User: NextPage = () => {
 
   useEffect(() => {
     if (!initData) {
-      fetchData("/api/v1/account/all", setInitData).catch((err) => {
+      fetchInitUserData("/api/v1/account/all", setInitData).catch((err) => {
         PrintErrorMessage(err);
       });
-    }
-    if (initData) {
-      createUserInformation(initData);
+      return;
     }
   }, [initData]);
 
@@ -89,7 +53,7 @@ const User: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GlobalNavigationBar />
-      {userInformation ? (
+      {initData ? (
         <DataGrid
           columns={dataColumn}
           rows={dataRow}

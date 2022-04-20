cf1aede454b7df66cee27f29cc77b89b3cee4ab7
wook <gus5427@naver.com>
2022-04-20 14:57:50 +0900

    Feat : add fetchDataOnBrowser functions in basicFetch.ts

--- api/basicFetch.ts
+++ api/basicFetch.ts
@@ -1,3 +1,10 @@
-export function fetchInBrowser(url: string) {
-  return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`);
+import React from "react";
+
+export function fetchDataOnBrowser(url: string, setData: React.Dispatch<any>) {
+  return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`).then((res) => {
+    res.json().then((value) => {
+      setData(value);
+    });
+    console.log("setting Done!");
+  });
 }

797856ec9b89594814591c73ed138ce990b1e43f
wook <gus5427@naver.com>
2022-04-20 20:42:12 +0900

    Feat : add GridColumn in constant.ts

--- util/constant.ts
+++ util/constant.ts
@@ -2,3 +2,18 @@ export enum ClickType {
   SINGLE = 1,
   DOUBLE = 2,
 }
+
+export const GridColumn = {
+  User: [
+    { field: "displayName", headerName: "닉네임", width: 200 },
+    { field: "email", headerName: "이메일", width: 200 },
+    { field: "phoneNumber", headerName: "전화번호", width: 120 },
+    { field: "birthDay", headerName: "생년월일" },
+    // { field: "bloodType", headerName: "혈액형" },
+    { field: "gender", headerName: "성별" },
+    { field: "marketingUserAgreement", headerName: "마케팅활용동의" },
+    { field: "status", headerName: "상태" },
+    { field: "createdAt", headerName: "가입날짜" },
+    { field: "uuid", headerName: "사용자 식별자", width: 300 },
+  ],
+};

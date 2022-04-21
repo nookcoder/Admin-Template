cc59656b1fd7d0ba73137f63af4a4b0f21997b56
wook <gus5427@naver.com>
2022-04-21 21:25:39 +0900

    Feat : add IDetailComponent.ts

---
+++ model/interface/common/IDetailComponent.ts
@@ -0,0 +1,11 @@
+export interface IDetailTextField {
+  defaultValue: string;
+  label: string;
+  state: string;
+}
+
+export interface IDetailTextArea {
+  defaultValue: string;
+  label: string;
+  state: string;
+}

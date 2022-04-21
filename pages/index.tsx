2d86384123501a4852fc054aee9ac077251be633
wook <gus5427@naver.com>
2022-04-21 21:28:52 +0900

    Remove : remove GlobalNavigationBar in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React from "react";
-import GlobalNavigationBar from "../components/common/GlobalNavigationBar";
 
 const Home: NextPage = () => {
   return (
@@ -12,8 +11,6 @@ const Home: NextPage = () => {
         <meta name="description" content="피플 Admin 웹사이트" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <GlobalNavigationBar />
     </div>
   );
 };

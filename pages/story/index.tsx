ff50e051897075577ad6b440dee034c2b89e8fd2
wook <gus5427@naver.com>
2022-04-21 18:02:49 +0900

    Feat : add DataGrid in story/index.tsx

--- pages/story/index.tsx
+++ pages/story/index.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
 import GlobalNavigationBar from "../../components/common/GlobalNavigationBar";
 import Head from "next/head";
+import { GridColumn } from "../../util/constant";
+import { fetchWithBaseURL } from "../../api/basicFetch";
+import { IDonation, IDonationContent } from "../../model/interface/IDonation";
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { setGridPropsRow } from "../../hooks/gridPropsRowHook";
+import { CircularProgress } from "@mui/material";
 
-const Story: NextPage = () => {
+const Story: NextPage<IDonation> = ({ data }) => {
+  const [content, setContent] = useState<Array<IDonationContent>>();
+  const dataColum: GridColDef[] = GridColumn.Story;
+  const dataRow: GridRowsProp = content ? content : [];
+
+  useEffect(() => {
+    setGridPropsRow(data.content, setContent);
+  }, [data]);
+
   return (
     <div>
       <Head>
@@ -12,9 +26,21 @@ const Story: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GlobalNavigationBar />
-      story 페이지
+      {content ? (
+        <DataGrid columns={dataColum} rows={dataRow} autoHeight={true} />
+      ) : (
+        <CircularProgress />
+      )}
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await fetchWithBaseURL("/api/v1/donation");
+  const data = await res.json();
+  return {
+    props: { data },
+  };
+}
 
 export default Story;

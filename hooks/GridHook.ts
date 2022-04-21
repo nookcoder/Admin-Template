1107a6f55cda57b7d092756c49c2909596329e85
wook <gus5427@naver.com>
2022-04-22 02:12:04 +0900

    Feat : add alerting server error message in setGridPropsRow.ts

--- hooks/GridHook.ts
+++ hooks/GridHook.ts
@@ -1,16 +1,20 @@
 import React from "react";
 
 export function setGridPropsRow(
-  data: Array<any>,
+  data: Array<any> | object,
   setData: React.Dispatch<any>,
 ) {
-  const newDataArray: Array<any> = [];
-  data.map((data, id) => {
-    const newData = {
-      ...data,
-      id: id + 1,
-    };
-    newDataArray.push(newData);
-  });
-  setData(newDataArray);
+  if (data instanceof Array) {
+    const newDataArray: Array<any> = [];
+    data.map((data, id) => {
+      const newData = {
+        ...data,
+        id: id + 1,
+      };
+      newDataArray.push(newData);
+    });
+    setData(newDataArray);
+    return;
+  }
+  alert("서버에러");
 }

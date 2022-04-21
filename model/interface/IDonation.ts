7fe8e4761affa77debc858ed44c6c7d16393aeb6
wook <gus5427@naver.com>
2022-04-21 17:36:16 +0900

    Feat : create IDonation.ts

---
+++ model/interface/IDonation.ts
@@ -0,0 +1,112 @@
+export interface IDonation {
+  data: {
+    content: [
+      {
+        bloodProduct: string;
+        content: string;
+        createdAt: string;
+        lastRenewedAt: string;
+        lettersCount: number;
+        likesCount: number;
+        modifiedAt: string;
+        patient: {
+          bloodType: {
+            abo: string;
+            rh: string;
+          };
+          createdAt: string;
+          modifiedAt: string;
+          status: string;
+          uuid: string;
+        };
+        phoneNumber: string;
+        profileImageUrl: string;
+        renewCount: number;
+        replyCount: number;
+        status: string;
+        title: string;
+        uuid: string;
+        viewsCount: number;
+        writer: {
+          accountUuid: string;
+          birthDay: string;
+          displayName: string;
+          email: string;
+          fcmToken: string;
+          gender: string;
+          phoneNumber: string;
+          profileImageUrl: string;
+          providerType: string;
+          role: string;
+          status: string;
+        };
+      },
+    ];
+    empty: boolean;
+    first: boolean;
+    last: boolean;
+    number: number;
+    numberOfElements: number;
+    pageable: {
+      offset: number;
+      pageNumber: number;
+      pageSize: number;
+      paged: boolean;
+      sort: {
+        empty: boolean;
+        sorted: boolean;
+        unsorted: boolean;
+      };
+      unpaged: boolean;
+    };
+    size: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    totalElements: number;
+    totalPages: number;
+  };
+}
+
+export interface IDonationContent {
+  bloodProduct: string;
+  content: string;
+  createdAt: string;
+  lastRenewedAt: string;
+  lettersCount: number;
+  likesCount: number;
+  modifiedAt: string;
+  patient: {
+    bloodType: {
+      abo: string;
+      rh: string;
+    };
+    createdAt: string;
+    modifiedAt: string;
+    status: string;
+    uuid: string;
+  };
+  phoneNumber: string;
+  profileImageUrl: string;
+  renewCount: number;
+  replyCount: number;
+  status: string;
+  title: string;
+  uuid: string;
+  viewsCount: number;
+  writer: {
+    accountUuid: string;
+    birthDay: string;
+    displayName: string;
+    email: string;
+    fcmToken: string;
+    gender: string;
+    phoneNumber: string;
+    profileImageUrl: string;
+    providerType: string;
+    role: string;
+    status: string;
+  };
+}

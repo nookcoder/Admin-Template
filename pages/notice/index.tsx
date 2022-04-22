1dc832163eb48bd0665d51a1fab488ad110e3708
wook <gus5427@naver.com>
2022-04-22 21:23:31 +0900

    Temp : create route to /notice/create

--- pages/notice/index.tsx
+++ pages/notice/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { NextPage } from "next";
-import GlobalNavigationBar from "../../components/common/GlobalNavigationBar";
 import Head from "next/head";
+import "react-quill/dist/quill.snow.css";
+import { useRouter } from "next/router";
+import { Button } from "@mui/material";
 
 const Notice: NextPage = () => {
+  const router = useRouter();
+  const routeCreatingNoticePage = () => {
+    router.push("/notice/create");
+  };
+
   return (
     <div>
       <Head>
@@ -11,8 +18,10 @@ const Notice: NextPage = () => {
         <meta name="description" content="피플 Admin 공지사항" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <GlobalNavigationBar />
       공지사항 페이지
+      <Button variant={"contained"} onClick={routeCreatingNoticePage}>
+        공지 글 등록하기
+      </Button>
     </div>
   );
 };

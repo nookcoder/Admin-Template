288b011fd8d5910483277011a0cc694600c04898
wook <gus5427@naver.com>
2022-04-25 16:30:16 +0900

    TEST : add testing code for admin Login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,42 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const [token, setToken] = useState<string>("");
+
+  const onClick = () => {
+    router.push("/login");
+  };
+
+  const onClickAdminTest = async () => {
+    const tokenFromUrl = router.query;
+    if (tokenFromUrl.token && typeof tokenFromUrl.token == "string") {
+      await setToken(tokenFromUrl.token);
+      console.log(tokenFromUrl.token);
+      console.log("Success!");
+    }
+
+    axios
+      .get(`${process.env["NEXT_PUBLIC_ADMIN_TEST"]}`, {
+        headers: {
+          "X-AUTH-TOKEN": `${tokenFromUrl.token}`,
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,6 +44,10 @@ const Home: NextPage = () => {
         <meta name="description" content="피플 Admin 웹사이트" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Button onClick={onClick}>로그인 Test</Button>
+      <Button variant={"contained"} onClick={onClickAdminTest}>
+        Admin Test
+      </Button>
     </div>
   );
 };

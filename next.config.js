4fd8fe1041827cfd0f689bad9ccc9fd7d2df8b71
wook <gus5427@naver.com>
2022-04-26 22:44:32 +0900

    Fix : update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,24 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  headers: [
+    {
+      source: "/api/*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;

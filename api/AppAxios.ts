75038588d2fe69a7ddeb2bbaa69887f4b3c58a28
wook <gus5427@naver.com>
2022-04-29 15:46:52 +0900

    FEAT : create AppAxios.ts

---
+++ api/AppAxios.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const appAxios = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+});
+
+export function appAxiosPost(url: string, body: any, accessToken: string) {
+  return appAxios.post(url, body, {
+    headers: {
+      "X-AUTH-TOKEN": `${accessToken}`,
+    },
+  });
+}
+
+export function appAxiosGet(url: string, accessToken: string) {
+  return appAxios.get(url, {
+    headers: {
+      "X-AUTH-TOKEN": `${accessToken}`,
+    },
+  });
+}

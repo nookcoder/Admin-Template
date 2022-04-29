217e0c819b72af3b1d0ae506fc9e2f54f65dfeb5
wook <gus5427@naver.com>
2022-04-29 16:15:27 +0900

    FEAT : add function of redux-persist

--- redux/store.ts
+++ redux/store.ts
@@ -1,16 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./feature/detail/userSlice";
 import donationSlice from "./feature/detail/donationSlice";
-import noticeSlice from "./feature/notice/noticeSlice";
 import authSlice from "./feature/auth/authSlice";
+import noticeSlice from "./feature/notice/noticeSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const reducers = combineReducers({
+  detailUser: userSlice,
+  detailDonation: donationSlice,
+  notice: noticeSlice,
+  auth: authSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const pReducers = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    detailUser: userSlice,
-    detailDonation: donationSlice,
-    notice: noticeSlice,
-    auth: authSlice,
-  },
+  reducer: pReducers,
 });
 
 export type RootState = ReturnType<typeof store.getState>;

4cac7105675632f9daad6072131b1debd28d05c2
wook <gus5427@naver.com>
2022-05-11 14:17:09 +0900

    FEAT : add updating Event Content

--- pages/event/[uuid].tsx
+++ pages/event/[uuid].tsx
@@ -10,26 +10,39 @@ import {
   InferGetServerSidePropsType,
   NextPage,
 } from "next";
-import { fetchWithBaseURL } from "../../api/AppFetch";
-import { IEvent } from "../../model/interface/event/IEvent";
-import DetailTextField from "../../components/common/DetailTextField";
 import { useAppDispatch, useAppSelect } from "../../hooks/ReduxHooks";
-import DetailTextArea from "../../components/common/DetailTextArea";
-import Image from "next/image";
 import { Button } from "@mui/material";
 import { FILE_TYPE } from "../../util/constant";
 import styles from "../../styles/EventUpdate.module.scss";
-import { setEventImageUrl } from "../../redux/feature/event/eventSlice";
+import {
+  setEventContent,
+  setEventImageUrl,
+  setEventTitle,
+} from "../../redux/feature/event/eventSlice";
+import { IEvent } from "../../model/interface/event/IEvent";
+import DetailTextArea from "../../components/common/DetailTextArea";
+import DetailTextField from "../../components/common/DetailTextField";
+import { auto } from "@popperjs/core";
+import { loadEvent } from "../../lib/fetch-events";
+import { appAxiosPatch } from "../../api/AppAxios";
+import { useRouter } from "next/router";
+import { PrintErrorMessage } from "../../util/Error";
 
 const EventDetail: NextPage<IEvent> = ({
-  eventDetail,
+  event,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const dispatch = useAppDispatch();
+  const router = useRouter();
+  const uploadFileInput = useRef<HTMLInputElement>(null);
+
+  const [uuid, setUuid] = useState<string>(event.uuid);
+  const [imageFile, setImageFile] = useState<File>();
+  const [eventDetail, setEventDetail] = useState<IEvent>();
+
   const title = useAppSelect((state) => state.event.title);
   const content = useAppSelect((state) => state.event.content);
   const imageSourceUrl = useAppSelect((state) => state.event.imageSourceUrl);
-  const [imageFile, setImageFile] = useState<File>();
-  const uploadFileInput = useRef<HTMLInputElement>(null);
+  const accessToken = useAppSelect((state) => state.auth.accessToken);
 
   const onClickFileUploadElement = () => {
     if (uploadFileInput && uploadFileInput.current) {
@@ -51,69 +64,92 @@ const EventDetail: NextPage<IEvent> = ({
     const bodyDto = new FormData();
     bodyDto.set("title", title);
     bodyDto.set("content", content);
-    imageFile
-      ? bodyDto.set("uploadImageFile", imageFile, imageFile.name)
-      : bodyDto.set("uploadImageFile", "");
+    bodyDto.set("eventUuid", uuid);
+    if (imageFile) {
+      bodyDto.set("uploadImageFile", imageFile, imageFile.name);
+    }
+
+    appAxiosPatch("/api/v1/admin/pple/event", bodyDto, accessToken)
+      .then((res) => {
+        router.push("/event");
+      })
+      .catch((err) => {
+        PrintErrorMessage(err.status);
+      });
   };
 
   useEffect(() => {
-    dispatch(setEventImageUrl(eventDetail.eventImageUrl));
-  }, [dispatch, eventDetail.eventImageUrl]);
+    setEventDetail(event);
+    dispatch(setEventTitle(event.title));
+    dispatch(setEventContent(event.content));
+    dispatch(setEventImageUrl(event.eventImageUrl));
+  }, [event, dispatch]);
 
   return (
     <div>
-      <form action="">
-        <main className={styles.container}>
-          <div className={styles.box}>
-            <DetailTextField
-              defaultValue={eventDetail.title}
-              label={"이벤트 제목"}
-              state={title}
-            />
-            <DetailTextArea
-              defaultValue={eventDetail.content}
-              label={"이벤트 내용"}
-              state={content}
-            />
-
-            <Image
-              src={imageSourceUrl}
-              alt={"이미지"}
-              width={300}
-              height={300}
-            />
-          </div>
-          <div>
-            <h3>이미지 수정할 거면 이걸 클릭해주세용</h3>
-            <Button onClick={onClickFileUploadElement} variant={"contained"}>
-              Update Image
-              <input
-                ref={uploadFileInput}
-                type={"file"}
-                hidden={true}
-                accept={FILE_TYPE}
-                onChange={onChangeImage}
+      {eventDetail ? (
+        <form action="" onSubmit={onSubmit}>
+          <main className={styles.container}>
+            <div className={styles.box}>
+              <DetailTextField
+                defaultValue={eventDetail.title}
+                label={"이벤트 제목"}
+                state={title}
               />
+              <DetailTextArea
+                defaultValue={eventDetail.content}
+                label={"이벤트 내용"}
+                state={content}
+              />
+
+              <img
+                src={imageSourceUrl}
+                alt={"이미지"}
+                width={300}
+                height={auto}
+              />
+            </div>
+            <div>
+              <h3>이미지 수정할 거면 이걸 클릭해주세용</h3>
+              <Button onClick={onClickFileUploadElement} variant={"contained"}>
+                Update Image
+                <input
+                  ref={uploadFileInput}
+                  type={"file"}
+                  hidden={true}
+                  accept={FILE_TYPE}
+                  onChange={onChangeImage}
+                />
+              </Button>
+            </div>
+          </main>
+          <div className={styles.button_layout}>
+            <Button type={"submit"} variant={"contained"}>
+              수정하기
             </Button>
           </div>
-        </main>
-        <div className={styles.button_layout}>
-          <Button type={"submit"} variant={"contained"}>
-            수정하기
-          </Button>
-        </div>
-      </form>
+        </form>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const uuid = context.query["uuid"];
-  const res = await fetchWithBaseURL(`/api/v1/pple/event/${uuid}`);
-  const eventDetail: IEvent = await res.json();
+  const { uuid } = context.query;
+  if (!uuid) {
+    console.log("UUID 가 없습니다");
+  }
+  if (typeof uuid == "string") {
+    const event = await loadEvent(uuid);
+    return {
+      props: { event },
+    };
+  }
   return {
     props: {
-      eventDetail,
+      event: null,
     },
   };
 };

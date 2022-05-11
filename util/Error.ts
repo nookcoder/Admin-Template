b1bf93a337297fcbcd820caab075b7bb20ef5d0f
wook <gus5427@naver.com>
2022-05-11 14:17:41 +0900

    STYLE : update parameter type from string to number in Error.ts

--- util/Error.ts
+++ util/Error.ts
@@ -1,21 +1,18 @@
-import { AxiosError } from "axios";
-
-export const PrintErrorMessage = (error: AxiosError) => {
-  console.log(error.code);
-  switch (error.code) {
-    case "400":
+export const PrintErrorMessage = (errorCode: number) => {
+  switch (errorCode) {
+    case 400:
       alert("Error Code : 400 잘못된 요청입니다");
       break;
-    case "401":
+    case 401:
       alert("Error Code : 401 권한이 없습니다");
       break;
-    case "403":
+    case 403:
       alert("Error Code : 403 권한이 없습니다");
       break;
-    case "404":
+    case 404:
       alert("Error Code : 404 해당 페이지를 찾을 수 없습니다");
       break;
-    case "500":
+    case 500:
       alert("Error Code : 500 서버에러입니다. 관리자에게 문의해주세요");
       break;
     default:

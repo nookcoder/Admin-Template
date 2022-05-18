4c2e0058821384f1f2e73692994b5808d05f62de
wook <gus5427@naver.com>
2022-05-18 15:35:49 +0900

    FEAT : add User 기본 정보 in user/[uuid].tsx

--- pages/user/[accountUuid].tsx
+++ pages/user/[accountUuid].tsx
@@ -6,14 +6,12 @@ import IUser from "../../model/interface/user/IUser";
 import { fetchData } from "../../lib/api/AppFetch";
 import { PrintErrorMessage } from "../../util/Error";
 import styles from "../../styles/AccountDetail.module.scss";
-import DetailTextField from "../../components/common/DetailTextField";
 import { useAppSelect } from "../../hooks/ReduxHooks";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Divider } from "@mui/material";
 import Head from "next/head";
-import {
-  setBirthDay,
-  setDisplayName,
-} from "../../redux/feature/detail/userSlice";
+import UserDetailLeftBox from "../../components/user/UserDetailLeftBox";
+import UserDetailRightBox from "../../components/user/UserDetailRightBox";
+import TermsBox from "../../components/user/TermsBox";
 
 const AccountUuid: NextPage = () => {
   const router = useRouter();
@@ -43,22 +41,24 @@ const AccountUuid: NextPage = () => {
         <title>Pple | 유저 상세정보</title>
       </Head>
       {userData ? (
-        <div className={styles.box}>
-          <h2>유저 상세 정보 조회 및 수정 / 삭제</h2>
+        <>
+          <h2 className={styles.title}>유저 상세 정보 조회 및 수정 / 삭제</h2>
 
-          <DetailTextField
-            defaultValue={userData.birthDay}
-            label={"생년월일"}
-            state={birthDay}
-            slice={setBirthDay}
-          />
+          <main className={styles.box}>
+            <section aria-label={"왼쪽 컬럼"} className={styles.box_column}>
+              <UserDetailLeftBox userData={userData} />
+            </section>
 
-          <DetailTextField
-            defaultValue={userData.displayName}
-            label={"닉네임"}
-            state={displayName}
-            slice={setDisplayName}
-          />
+            <section aria-label={"오른쪽 컬럼"} className={styles.box_column}>
+              <UserDetailRightBox userData={userData} />
+              <TermsBox userData={userData} />
+            </section>
+          </main>
+
+          <Divider />
+
+          <main></main>
+
           <button
             onClick={() => {
               console.log(displayName + " ? " + birthDay);
@@ -66,7 +66,7 @@ const AccountUuid: NextPage = () => {
           >
             출력
           </button>
-        </div>
+        </>
       ) : (
         <CircularProgress></CircularProgress>
       )}
